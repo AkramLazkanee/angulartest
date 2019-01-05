f4aa69c3be0686f9f74164d30f7a6ed7f0ce20ae
akramlazkanee <akramlazkanee@github.com>
2019-01-05 11:41:55 +0200

    list init

--- chat/src/app/list/list.module.ts
+++ chat/src/app/list/list.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ConversationsService } from './conversations.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
+  providers: [ConversationsService],
   declarations: [ContactsListComponent]
 })
 export class ListModule { }
