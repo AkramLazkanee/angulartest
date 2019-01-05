e45953b2d748f243c253d042b83c1dd97121475b
omarz9 <omarz9@github.com>
2019-01-05 10:54:13 +0200

    init

--- chat/src/app/app.module.ts
+++ chat/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AuthModule,
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
