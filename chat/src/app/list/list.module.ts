063f6ee908f0575162bca756e921d002e9cbfa7d
akramlazkanee <akramlazkanee@github.com>
2019-01-05 12:54:41 +0200

    list routes

--- chat/src/app/list/list.module.ts
+++ chat/src/app/list/list.module.ts
@@ -4,10 +4,12 @@ import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { RowComponent } from './row/row.component';
 import { ContactsService } from './contacts.service';
 import { ConversationsService } from './conversations.service';
+import { ListRoutingModule } from './list-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ListRoutingModule
   ],
   providers:[ContactsService,ConversationsService],
   declarations: [ContactsListComponent, RowComponent]
