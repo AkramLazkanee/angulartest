3a3153e56327d8def06c5122ff51c96e4684cdae
adnanki208 <adnanki208@github.com>
2019-01-05 11:56:41 +0200

    service contacts

--- chat/src/app/list/contacts.service.ts
+++ chat/src/app/list/contacts.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Contact } from './contact';
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
 
-  constructor() { }
+  headers = new HttpHeaders();
+  // loggedInUser: loggedInUser = new loggedInUser();
+  isLoggedIn = false;
+
+  getHeaders() {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')/*.set('Authorization', 'Bearer ' + this.loggedInUser.token)*/;
+    return headers;
+  }
+  constructor(private http: HttpClient) {
+    this.headers = this.getHeaders();
+  }
+  getContactsList(): Observable<Contact[]> {
+    return this.http.get<Contact[]>(environment.api_url + 'api/Contacts', {
+      headers: this
+        .headers
+    })
+  }
 }
