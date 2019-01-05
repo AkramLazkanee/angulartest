f0ea4018ba0ff2a6ae12d742bca15044e256dbd0
adnanki208 <adnanki208@github.com>
2019-01-05 11:05:31 +0200

    init

---
+++ chat/src/app/list/list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ContactsListComponent]
+})
+export class ListModule { }
