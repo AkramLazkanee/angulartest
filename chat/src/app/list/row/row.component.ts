b99f566eb8803b341fa6a4aafce1d7317f8abeee
Tareq <tareq7777@gmail.com>
2019-01-12 12:26:55 +0200

    update

--- chat/src/app/list/row/row.component.ts
+++ chat/src/app/list/row/row.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Contact } from '../contact';
 import { Conversation } from '../conversation';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-row',
@@ -12,12 +13,18 @@ export class RowComponent implements OnInit {
   @Input('contact') contact: Contact;
   @Input('conversation') conversation: Conversation;
   item;
-  constructor() { }
+  constructor( private router:Router) { }
 
   ngOnInit() {
     this.item = this.contact ? this.contact : this.conversation;
     // console.log(this.item);
 
   }
+
+  showConvo() {
+    this.router.navigate( ['/list/conversations', this.item.Id] );
+    console.log(this.item)
+  }
+
 
 }
